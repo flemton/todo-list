cb56c89ad1a1f4dcc484c215a5cf17a1fb1bb2f1
Alhassan Osman <oalhassan847@gmail.com>
2023-01-19 22:27:48 +0000

    setup linters

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
-import _ from 'lodash';
 import './style.css';
 
 const Tasks = [
-    {
-        description: "wash the dishes",
-        completed: true,
-        index: 0
-    },
-    {
-        description: "complete To Do list project",
-        completed: true,
-        index: 1
-    },
-]
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'complete To Do list project',
+    completed: true,
+    index: 1,
+  },
+];
 
 const listSection = document.querySelector('.todo-list');
 
-Tasks.forEach((Task) => 
-{
-    const task = `
+Tasks.forEach((Task) => {
+  const task = `
     <li id="${Task.index}" class="${Task.completed}">
     <input class="to-do-input" type="checkbox">
     <p>${Task.description}</p>
     <i class="fa-solid fa-ellipsis-vertical"></i>
     </li>
-    <hr>`
-    listSection.innerHTML += task;
-
-})
+    <hr>`;
+  listSection.innerHTML += task;
+});
