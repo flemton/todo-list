0673b88c26a16ef678b43460672a8f59c1881ac2
Alhassan Osman <oalhassan847@gmail.com>
2023-01-20 00:56:59 +0000

    add modules directory
    Add modules folder and starting to separate index.js into several modules and implement functionality

---
+++ modules/addList.js
@@ -0,0 +1,7 @@
+export default class AddList {
+    constructor(input) {
+      this.description = input;
+      this.completed = false;
+      this.index = 1;
+    }
+  }
---
+++ modules/showLists.js
@@ -0,0 +1,23 @@
+const listSection = document.querySelector('.todo-list');
+
+export default function showLists() {
+  const storedListData = JSON.parse(localStorage.getItem('todo-list'));
+  let status = '';
+  listSection.innerHTML = '';
+  storedListData.forEach((Task) => {
+    if (Task.complete === true) {
+      status = 'checked';
+    } else {
+      status = '';
+    }
+    listSection.innerHTML += `
+    <li id="${Task.index}" class="${Task.completed}">
+    <input class="to-do-input" type="checkbox">
+    <p>${Task.description}</p>
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    </li>
+    <hr>`;
+  });
+  inputChecker();
+  edit();
+}
