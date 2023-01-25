f2d432db8de9cd2ef129912a389e4af801606077
Alhassan Osman <oalhassan847@gmail.com>
2023-01-25 17:03:46 +0000

    add flexbox class

--- modules/completed.js
+++ modules/completed.js
@@ -5,7 +5,7 @@ export default function completed() {
       const restoredListData = JSON.parse(localStorage.getItem('todo-list'));
       const Element = e.target;
       const Id = Element.id;
-      if (Element.checked === true) {
+      if (Element.checked) {
         restoredListData[Id - 1].completed = true;
       } else {
         restoredListData[Id - 1].completed = false;
